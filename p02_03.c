729efb08a766b7c6e7b6ae517751c6afd2291b35
ferdinicolas07-ux <ferdinicolas07@gmail.com>
2026-03-14 10:20:14 +0700

    done

--- p02_03.c
+++ p02_03.c
@@ -26,7 +26,7 @@ int main() {
         printf("Masukkan Jumlah: ");
         scanf("%d", &jumlah);
 
-        switch (pilihan) {
+        switch (Pilihan) {
             case 1:
                 total += 4000 * jumlah;
                 break;
